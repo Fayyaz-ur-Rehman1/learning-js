81ba8d6a1d494c647c06e5522a1aef6928b6daa8
FAYYAZ-UR-REHMAN <111459002+Fayyaz-ur-Rehman1@users.noreply.github.com>
2022-11-05 22:06:51 +0530

    Add files via upload

---
+++ js/function.js
@@ -0,0 +1,37 @@
+function table(){
+    document.write("Drow-Table <br>");
+}
+table()
+var a = 1;
+while(a <= 100){
+    document.write("my name is fayyaz khilji <br>")
+    a++
+}
+table()
+document.write("<br>")
+table()
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+
+var a = 20
+if (20 > 10) {
+    document.write("this is write answer")
+} else{
+    document.write("this is a worng answer")
+}
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+table()
+var b = 100
+var c = 10
+var d = (100 < 10)
+alert("thanks")
+alert("sorry")
+table()
+table()
+table()
---
+++ js/loop.js
@@ -0,0 +1,25 @@
+ for(let i = 0 ; i < 10; i++){
+    console.log(i)
+}
+for(let j = 10 ; j < 100; j++){
+    console.log(j)
+}
+for(let i = 0 ; i < 10; ++i){  // console.log((i+1)*2)
+}
+const arr = [1, 'str', true]
+//console.log(arr.length)
+for(i = 0; i < arr.length;i++){
+    console.log(arr[i])
+} 
+
+
+
+
+
+
+
+/*function multiple(a , b){
+    return (a * b)
+}
+const a = multiple(12 , 2)
+console.log(a);*/
